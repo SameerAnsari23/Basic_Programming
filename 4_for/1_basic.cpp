f2cf1c0cdb2df08e386de03fc0b6eafededf0f73
Sameer Ansari <sameeransariscience@gmail.com>
2026-09-11 13:59:09 +0530

    For loop

---
+++ 4_for/1_basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // for (intialization; condition; updation) {}
+
+
+    for (int cnt = 1; cnt <= 100; cnt++) {
+        cout << "1 ";
+    }
+    
+    return 0;
+}
